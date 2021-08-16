a508f1cc7bb7557f8a98232d657c80216b9d0f16
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-17 00:52:31 +0300

    Updated dependencies

--- project.mk
+++ project.mk
@@ -21,7 +21,7 @@
 # Package version
 ARTIFACT_ID                 = LSP_PLUGINS_CROSSOVER
 ARTIFACT_NAME               = lsp-plugins-crossover
-ARTIFACT_DESC               = Trigger plugin series
+ARTIFACT_DESC               = Crossover plugin series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
 ARTIFACT_VERSION            = 1.0.1-devel
