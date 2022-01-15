a3861d4ed1644f172b9c8321f297682759efd63f
sadko4u <sadko4u@gmail.com>
2022-01-16 01:41:26 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-crossover.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_CROSSOVER
-ARTIFACT_NAME               = lsp-plugins-crossover
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Crossover Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_CROSSOVER
+PLUGIN_PACKAGE_NAME     = lsp-plugins
