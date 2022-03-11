abc0b8e408c07a850438cf0a15b4a2fc8edfeb0b
sadko4u <sadko4u@gmail.com>
2022-03-11 20:23:40 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -19,5 +19,11 @@
 #
 
 # Variables that describe dependencies
-PLUGIN_DEPENDENCIES     = LSP_PLUGINS_CROSSOVER
 PLUGIN_PACKAGE_NAME     = lsp-plugins
+
+PLUGIN_SHARED = \
+  LSP_PLUGINS_SHARED
+
+PLUGIN_DEPENDENCIES = lsp-plugins-crossover
+  LSP_PLUGINS_PLUGIN_TEMPLATE
+
