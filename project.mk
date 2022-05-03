12d1394af3731be00da5fcc768e6731d5099097a
sadko4u <sadko4u@gmail.com>
2022-05-03 23:34:24 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-crossover
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Crossover Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.2-devel
+ARTIFACT_VERSION            = 1.0.2
 
 
 
