03005b7ae23fd954ca8870621674e7b21b57fa44
sadko4u <sadko4u@gmail.com>
2022-05-29 23:16:38 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -25,3 +25,6 @@ ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Crossover Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_VERSION            = 1.0.4-devel
+
+
+
