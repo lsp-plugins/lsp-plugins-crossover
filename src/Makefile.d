67616a85f6f18541db72325d32649325758af55d
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-05 01:54:54 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -28,13 +28,6 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/plug/crossover.o: \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/status.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/midi.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/runtime/version.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/types.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/forge.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/parse.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/pattern.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/debug.h \
- $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/debug.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/stdlib/string.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/IPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/ICanvas.h \
@@ -43,6 +36,7 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/plug/crossover.o: \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/types.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/version.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/spec.h \
+ $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/new.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/ICanvasFactory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/IWrapper.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/core/KVTStorage.h \
@@ -100,6 +94,7 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/plug/crossover.o: \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/fmop_kx.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/fmop_vv.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/log.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/lramp.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/minmax.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/normalize.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/op_kx.h \
@@ -118,6 +113,7 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/plug/crossover.o: \
  $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/misc/windows.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/alloc.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/stdlib/stdlib.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/debug.h \
  $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/misc/envelope.h \
  $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/units.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/stdlib/math.h \
@@ -136,13 +132,6 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/ui/crossover.o: main/ui/crossover.cpp \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/status.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/midi.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/runtime/version.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/types.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/forge.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/parse.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/pattern.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/protocol/osc/debug.h \
- $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/debug.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/stdlib/string.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/IPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/ICanvas.h \
@@ -151,6 +140,7 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/ui/crossover.o: main/ui/crossover.cpp \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/types.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/version.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/spec.h \
+ $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/new.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/ICanvasFactory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/IWrapper.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/core/KVTStorage.h \
@@ -208,6 +198,7 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/ui/crossover.o: main/ui/crossover.cpp \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/fmop_kx.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/fmop_vv.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/log.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/lramp.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/minmax.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/normalize.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/op_kx.h \
@@ -438,6 +429,8 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/ui/crossover.o: main/ui/crossover.cpp \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Padding.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Enum.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Direction.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Layout.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/TextLayout.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/Widget.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/Registry.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/Factory.h \
