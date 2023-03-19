8274d796ac68d4f58f1a13be07d2e5e14924226b
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:17:55 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -410,6 +410,7 @@ $(LSP_PLUGINS_CROSSOVER_BIN)/main/ui/crossover.o: main/ui/crossover.cpp \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
