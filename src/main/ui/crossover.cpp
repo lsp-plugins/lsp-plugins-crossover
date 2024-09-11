5b5d5a75a6a1460a269a465d9bfd189b456a31d2
bor-T <bor-tattoo@ya.ru>
2024-09-11 20:42:25 +0300

    Changed the numbering.
    The numbering starts with one, at the UI level.
    Updated screenshots.

--- src/main/ui/crossover.cpp
+++ src/main/ui/crossover.cpp
@@ -268,7 +268,7 @@ namespace lsp
                 else
                     lc_string.set("lists.crossover.splits.index.split_id");
 
-                lc_string.params()->set_int("id", (vSplits.index_of(s) % (meta::crossover_metadata::BANDS_MAX-1))+1);
+                lc_string.params()->set_int("id", (vSplits.index_of(s) % (meta::crossover_metadata::BANDS_MAX-1))+2);
                 lc_string.format(&text);
                 params.set_string("id", &text);
                 lc_string.params()->clear();
