fc7c25410fa63cfe376797d6e2e98efae807bfde
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-04 23:41:57 +0300

    Using new mechanism for registering plugin metadata

--- src/main/meta/crossover.cpp
+++ src/main/meta/crossover.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2026 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2026 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugins-crossover
  * Created on: 3 авг. 2021 г.
@@ -20,6 +20,7 @@
  */
 
 #include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <lsp-plug.in/plug-fw/meta/registry.h>
 #include <lsp-plug.in/shared/meta/developers.h>
 #include <private/meta/crossover.h>
 
@@ -490,6 +491,7 @@ namespace lsp
             xover_mono_port_groups,
             &crossover_bundle
         };
+        LSP_REGISTER_METADATA(crossover_mono);
 
         const meta::plugin_t  crossover_stereo =
         {
@@ -520,6 +522,7 @@ namespace lsp
             xover_stereo_port_groups,
             &crossover_bundle
         };
+        LSP_REGISTER_METADATA(crossover_stereo);
 
         const meta::plugin_t  crossover_lr =
         {
@@ -550,6 +553,7 @@ namespace lsp
             xover_stereo_port_groups,
             &crossover_bundle
         };
+        LSP_REGISTER_METADATA(crossover_lr);
 
         const meta::plugin_t  crossover_ms =
         {
@@ -580,5 +584,7 @@ namespace lsp
             xover_ms_port_groups,
             &crossover_bundle
         };
+        LSP_REGISTER_METADATA(crossover_ms);
+
     } /* namespace meta */
 } /* namespace lsp */
